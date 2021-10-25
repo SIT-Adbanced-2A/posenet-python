13a0fd58b16f322e3c09c57c6e57ed57a766674c
aoba <65536toaru@gmail.com>
2021-10-25 20:05:51 +0900

    new file:   thread_test.py

---
+++ thread_test.py
@@ -0,0 +1,19 @@
+import time
+import threading
+
+def first():
+    print("1st")
+    time.sleep(3)
+    thread2 = threading.Thread(target=second)
+    thread2.start()
+
+def second():
+    print("2nd")
+    time.sleep(3)
+
+
+for i in range(10):
+    thread1 = threading.Thread(target=first)
+    thread1.start()
+    thread1.join()
+print("end threads")
